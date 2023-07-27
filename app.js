2079a048b4ee1b148017648c683cd1db480420cd
ZairDinahet <dinahetzair@gmail.com>
2023-07-26 23:14:40 -0300

    Estructura de proyecto node.js/espress armada

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 8080
+
+
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/index.html"))
+
+})
+
+
+
+app.listen(PORT, () =>{
+  console.log(`[server] levantando en el puerto ${PORT}`);
+})
