7cb1f656eaedc9903f498b07563d31556ad92602
rociocala <rpciocala@gmail.com>
2023-08-05 20:15:07 -0300

    agregue el register y maqueta inicial de login

--- app.js
+++ app.js
@@ -12,8 +12,12 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, "./views/index.html"))
 
 })
-
-
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/register.html"))
+})
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/login.html"))
+})
 
 app.listen(PORT, () =>{
   console.log(`[server] levantando en http://localhost:${PORT}`);
