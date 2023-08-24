f9f424809dddd0e4c342ea7bbe3e3dfa3e52ed8a
ZairDinahet <dinahetzair@gmail.com>
2023-08-23 22:11:24 -0300

    Se creo la carpeta data con un json para los libros

--- src/controllers/detailController.js
+++ src/controllers/detailController.js
@@ -1,3 +1,5 @@
+// en  books.json faltan completar las propiedades de description y biographyAutor. Hay que investigar y completarlas en el json
+
 const detailController = {
   index: function (req, res) {
     res.render('products/detail')
