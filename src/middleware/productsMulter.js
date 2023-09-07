471709259a5aa2d24b43897cf78a40396dc0ec62
Nahuel Sanchez <nahuelsanchez0899@gmail.com>
2023-09-06 21:21:06 -0300

    agregado la carpeta products para las imagenes

--- src/middleware/productsMulter.js
+++ src/middleware/productsMulter.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, path.join(__dirname, '../../public/img/home'))
+      cb(null, path.join(__dirname, '../../public/img/products'))
     },
 
     filename: function (req, file, cb) {
