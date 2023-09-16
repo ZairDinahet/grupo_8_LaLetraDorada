b70716c2cdfcf1749a9ce4e70d6ceeea09a3db17
ZairDinahet <dinahetzair@gmail.com>
2023-09-16 01:10:29 -0300

    feature/login and validations

---
+++ src/middleware/userLoginValdation.js
@@ -0,0 +1,10 @@
+const { body } = require('express-validator')
+
+const userLoginValidation = [
+  body('email')
+  .notEmpty().withMessage('Tienes que escribir un correo electrónico').bail()
+  .isEmail().withMessage('Debes escribir un formato de correo válido'),
+  body('password').notEmpty().withMessage('Tienes que escribir una contraseña'),
+]
+
+module.exports = userLoginValidation
